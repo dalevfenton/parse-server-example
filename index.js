87045e7f95bd0a5357eeac451ad66147bc277e7e
Dale Fenton <fentondale@gmail.com>
2016-03-31 15:56:34 -0400

    added S3Adapter include

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var S3Adapter = require('parse-server').S3Adapter;
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
@@ -18,9 +19,7 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   restAPIKey: process.env.REST_API_KEY || '',
-  console.log(process.env.S3_ACCESS_KEY);
-  console.log(process.env.S3_SECRET_KEY);
-  console.log(process.env.S3_BUCKET);
+
   filesAdapter: new S3Adapter(
     process.env.S3_ACCESS_KEY,
     process.env.S3_SECRET_KEY,
