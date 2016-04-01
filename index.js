706f3370fcc944a060cc10d2a8e04427c9530b2a
Dale Fenton <fentondale@gmail.com>
2016-04-01 00:22:40 -0400

    trying to fix things

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
-console.log(process.env.S3_ACCESS_KEY);
-console.log(process.env.S3_SECRET_KEY);
-console.log(process.env.S3_BUCKET);
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
